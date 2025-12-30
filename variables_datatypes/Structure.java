dd3031ca73c99ea06af553997a40447d890e35ce
ACM <223bhakti0079@dbit.in>
2025-12-31 02:48:26 +0530

    java boilerplate code

--- variables_datatypes/Structure.java
+++ variables_datatypes/Structure.java
@@ -12,3 +12,17 @@ public class Structure{    //class
 //      RUN COMMANDS
 //      javac variables_datatypes\Structure.java
 //      java variables_datatypes.Structure 
+//      ln put space and print the next statement on next line ie Bhakti's first java program next-line
+//      or \n is used ie Bhakti's first java program\n , now next statement will be print on next line
+
+
+// System.out.println("Bhakti's first java program");
+// System.out.println("Bhakti's second java program");
+
+// o/p - Bhakti's first java program 
+//       Bhakti's second java program
+
+// System.out.print("Bhakti's first java program");
+// System.out.print("Bhakti's second java program");
+
+// o/p - Bhakti's first java program Bhakti's second java program
