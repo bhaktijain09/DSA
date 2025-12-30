0bdd359ca60201a89c51d5bbb2cdde7346077f66
ACM <223bhakti0079@dbit.in>
2025-12-31 02:12:37 +0530

    Java Boilerplate code

---
+++ variables_datatypes/Structure.java
@@ -0,0 +1,14 @@
+// JAVA BOILERPLATE CODE
+
+package variables_datatypes;    //package folder
+
+public class Structure{    //class
+    public static void main(String[] args){     //function
+        System.out.println("Bhakti's first java program");   //print statement
+    }
+}
+
+
+//      RUN COMMANDS
+//      javac variables_datatypes\Structure.java
+//      java variables_datatypes.Structure 
