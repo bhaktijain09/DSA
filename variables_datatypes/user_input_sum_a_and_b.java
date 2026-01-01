733ef87b321d80b754c47b868253a9048d72d9f6
ACM <223bhakti0079@dbit.in>
2026-01-01 23:38:48 +0530

    user input sum a and b

---
+++ variables_datatypes/user_input_sum_a_and_b.java
@@ -0,0 +1,17 @@
+package variables_datatypes;
+
+import java.util.*;
+
+public class user_input_sum_a_and_b {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
+
+
+
+
+    }
+}
