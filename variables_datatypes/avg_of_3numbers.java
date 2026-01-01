3278541d816c765117d70d46bac22d30f509ba40
ACM <223bhakti0079@dbit.in>
2026-01-02 01:32:22 +0530

    user input avg of 3 numbers

---
+++ variables_datatypes/avg_of_3numbers.java
@@ -0,0 +1,12 @@
+package variables_datatypes;
+import java.util.*;
+public class avg_of_3numbers {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+    int Avg = (a+b+c)/3;
+    System.out.println(Avg);
+    }
+}
