2674e51802fe464872c5c1cd8c65cd52bc008e29
ACM <223bhakti0079@dbit.in>
2026-01-01 19:00:14 +0530

    question on datatype sum a and b and single line and multiline comments is also discussed below

---
+++ variables_datatypes/sum_a_and_b.java
@@ -0,0 +1,23 @@
+// QUESTION
+package variables_datatypes;
+
+public class sum_a_and_b {
+    public static void main(String[] args){
+    int a = 9;
+    int b = 1;
+    int sum = a + b ;
+    System.out.println(sum);
+    }
+}
+
+// TOPIC 2 - COMMENTS
+
+//single line comment -" // cnhb hfb "
+//multiline comment - " /* hbch bcnebwh */ "
+
+// eg - bhakti jain in  2026                                    - single line comment
+
+/* eg - bhakti
+        jain                                                      //  - multiline comment 
+        in 
+        2026*/
