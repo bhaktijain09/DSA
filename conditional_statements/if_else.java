d4d1a5d11950ad9ee2e4711956158c57e2722c4e
ACM <223bhakti0079@dbit.in>
2026-01-04 19:57:43 +0530

    if-else and if-if-else loop

---
+++ conditional_statements/if_else.java
@@ -0,0 +1,61 @@
+package conditional_statements;
+
+
+//run each loop once at a time-
+
+// if-else statements:
+public class if_else {
+    public static void main(String[] args) {
+    
+    int age = 17;
+    if(age>=18){
+        System.out.println("The person should vote");
+    }
+    else {
+        System.out.println("The person should not vote");
+    }
+        }
+}
+
+// if-if-else statements:
+// public class if_else {                            //uncomment to run this loop
+//     public static void main(String[] args) {
+    
+//     int age = 17;
+//     if(age>=18){
+//         System.out.println("The person should vote");
+//     }
+//     if (age>13 && age<18){
+//         System.out.println("The person is teenager");
+//     }
+//     else {
+//         System.out.println("The person should not vote");
+//     }
+//         }
+// }
+
+//  if-else:
+/* 
+ if (condition) 
+    {
+    statement1
+    }
+else{
+    statement2
+    }
+*/
+
+// if-if-else:
+/* 
+if (condition1) 
+    {
+    statement1
+    }
+if (condition2)
+    {
+    statement2
+    }
+else{
+    statement3
+    }
+*/
