599bcca7be6336f7ad1b8f6808a5aac32d44db3d
ACM <223bhakti0079@dbit.in>
2026-01-04 20:05:27 +0530

    largest of 2 numbers using if else loop

---
+++ conditional_statements/largest_of_2_numbers_if_else.java
@@ -0,0 +1,14 @@
+package conditional_statements;
+
+public class largest_of_2_numbers_if_else {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 1;
+        if(a>b){
+System.out.println("The largest number is a where a = " + a);
+        }  
+        else {
+System.out.println("The largest number is b where b = " + b);
+        }  
+}
+}
