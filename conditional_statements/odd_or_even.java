70ed79ed8f8437781b55fc67cd3680706dafb329
ACM <223bhakti0079@dbit.in>
2026-01-04 20:26:48 +0530

    To print if the number entered is even or odd when user inputs number ie user scanner

---
+++ conditional_statements/odd_or_even.java
@@ -0,0 +1,15 @@
+package conditional_statements;
+import java.util.*;
+public class odd_or_even {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number%2==0){
+            System.out.println("Even number:" + number);
+        } 
+        else{
+             System.out.println("Odd number:" + number);
+        }
+
+    }
+}
