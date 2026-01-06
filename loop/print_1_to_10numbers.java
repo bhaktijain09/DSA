0163e8e994b723756d8cdc6eb3840d05687e8473
ACM <223bhakti0079@dbit.in>
2026-01-06 22:24:56 +0530

    print number from 1 to 10 using while loop

---
+++ loop/print_1_to_10numbers.java
@@ -0,0 +1,36 @@
+package loop;
+
+public class print_1_to_10numbers {
+    public static void main(String[] args) {
+        int num = 1;
+        while(num<=10){
+System.out.print(num);       
+num++;
+        }
+    }
+}
+
+
+// output-
+// 1
+// 2
+// 3 
+// 4 
+// 5 
+// 6 
+// 7 
+// 8 
+// 9 
+// 10
+
+// package loop;
+
+// public class print_1_to_10numbers {
+//     public static void main(String[] args) {
+//         int num = 1;
+//         while(num<=10){
+// System.out.print(num + " ");       // output - 1 2 3 4 5 6 7 8 9 10
+// num++;
+//         }
+//     }
+// }
