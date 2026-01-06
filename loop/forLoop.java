5cc101f3fe75ef70b91478d75ea3d8cb8f6e1799
ACM <223bhakti0079@dbit.in>
2026-01-06 23:49:20 +0530

    print square * pattern

---
+++ loop/forLoop.java
@@ -0,0 +1,21 @@
+package loop;
+
+public class forLoop {
+    public static void main(String[] args) {
+        int i;                             // variable declared
+        for (i=1; i<=10; i++){            // for (initialization; condition; updation){
+            System.out.println(i);       //  System.out.println(statement/ output); }
+        }
+    }
+    
+}
+
+// syntax
+
+/*  int i;                                           //declare variable 
+    for (initialization; condition; updation)
+    {
+        System.out.println(output statement); 
+    } 
+*/ 
+
---
+++ loop/print_square_pattern.java
@@ -0,0 +1,27 @@
+package loop;
+
+public class print_square_pattern {
+    public static void main(String[] args) {
+        int i;
+        for (i=1; i<=4; i++)
+        {
+            System.out.println("****");
+        }
+    }
+}
+
+// output - 
+
+// ****
+// ****
+// ****
+// ****
+
+
+//same output using while loop - 
+
+// int i=1;                                    //initialization
+// while (i<=4){                              //condition
+//   System.out.println("****"); //output statement
+//   i++;
+// }
