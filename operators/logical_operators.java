3bd5b30c39512ba85c27ad1bb44e65782c7e8bf0
ACM <223bhakti0079@dbit.in>
2026-01-07 18:59:00 +0530

    uncomment each para to run logical operators in this file to avoid conflict

--- operators/logical_operators.java
+++ operators/logical_operators.java
@@ -3,18 +3,18 @@ package operators;
 public class logical_operators {
     public static void main(String[] args) {
 
-
+// run each one by one by uncommenting
         //Logical AND (&&)
-        System.out.println( (3>2) && (5>4) );  //T&&T = T
-        System.out.println( (3>2) && (3>4) );  //T&&F = F
-        System.out.println( (1>2) && (5>4) );  //F&&T = F
-        System.out.println( (1>2) && (3>4) );  //F&&F = F
+        // System.out.println( (3>2) && (5>4) );  //T&&T = T
+        // System.out.println( (3>2) && (3>4) );  //T&&F = F
+        // System.out.println( (1>2) && (5>4) );  //F&&T = F
+        // System.out.println( (1>2) && (3>4) );  //F&&F = F
 
         //Logical OR (||)
-        System.out.println( (1>2) || (3>4) );  //F||F = F
-        System.out.println( (3>2) || (3>4) );  //T||F = T
-        System.out.println( (1>2) || (5>4) );  //F||T = T
-        System.out.println( (2>1) || (5>4) );  //T||T = T
+        // System.out.println( (1>2) || (3>4) );  //F||F = F
+        // System.out.println( (3>2) || (3>4) );  //T||F = T
+        // System.out.println( (1>2) || (5>4) );  //F||T = T
+        // System.out.println( (2>1) || (5>4) );  //T||T = T
         
         //Logical NOT (!) 
         System.out.println( !(5>1) );   //IS TRUE BUT RETURNS FALSE SINCE LOGICAL NOT (T but returns/gives F)
