ea6ac2a920d707bb5f0bdaff36094fd4ad2b32f9
ACM <223bhakti0079@dbit.in>
2026-01-08 22:47:25 +0530

    break statement to break the loop

---
+++ loop/break_statement.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class break_statement{
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<=5; i++){
+        if(i==3)
+        {
+        break;
+        }
+System.out.println(i);
+        }
+System.out.println("I am out of the loop");
+    }
+}
