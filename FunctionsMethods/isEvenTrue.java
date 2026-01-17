56cc21adddb5c25cf1e97dc8f7e0e180f8393ea1
ACM <223bhakti0079@dbit.in>
2026-01-17 23:19:02 +0530

    if the number entered is even, print true else false

---
+++ FunctionsMethods/isEvenTrue.java
@@ -0,0 +1,26 @@
+// Write a method named is Even that accepts an int argument.
+// The method should return true if the argument is even, or false otherwise. 
+// Also write a program to test your method.
+
+package FunctionsMethods;
+import java.util.*;
+
+public class isEvenTrue {
+    public static boolean isEven(int n){
+    return (n%2 == 0);
+    }
+       
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number n: ");
+        int n = sc.nextInt();
+
+       boolean result = isEven(n);
+       System.out.println("Is the number even? : "+ result); // if n is even: true else n is even: false
+
+       sc.close();
+    }
+    
+}
