823d6bd3544f61d8b900def00063b2d21a8123bb
ACM <223bhakti0079@dbit.in>
2026-01-22 21:32:25 +0530

    is Prime_optimized

---
+++ FunctionsMethods/Prime_optimized.java
@@ -0,0 +1,24 @@
+package FunctionsMethods;
+
+import java.util.Scanner;
+
+public class Prime_optimized {
+    public static boolean isPrime(int n){
+   
+        if(n == 2 ){
+            return true;
+        }
+
+        for(int i=2; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+               return false;
+            }
+        }
+        return true;
+    } 
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+    
+}
