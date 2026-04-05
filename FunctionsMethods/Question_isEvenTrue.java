f1d1bb4192f78044fa46454c2d2f0a98c9e2470d
ACM <223bhakti0079@dbit.in>
2026-04-05 23:47:48 +0530

    isEven accepts int argument and returns true/false(boolean) accordingly

---
+++ FunctionsMethods/Question_isEvenTrue.java
@@ -0,0 +1,20 @@
+// Write a method named isEven that accepts an int argument. 
+// The method should return true if the argument is even, or false otherwise. 
+// Also write a program to test your method.
+
+package FunctionsMethods;
+
+import java.util.Scanner;
+
+public class Question_isEvenTrue {
+    public static boolean isEven(int n) {
+        return n%2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter the number n:");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isEven(n));
+    }
+}
