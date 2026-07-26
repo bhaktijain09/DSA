fc4787b928f4aca67d979d35000f7583afe72f61
ACM <223bhakti0079@dbit.in>
2026-07-26 22:02:36 +0530

    Q4 - Find duplicates in array - leetcode 217. Contains Duplicate - HashSet optimal approach

---
+++ CAPGEMINI/Find_Duplicates_in_array.java
@@ -0,0 +1,33 @@
+import java.util.HashSet;
+// Brute force approach - 
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+
+//          for(int i=0; i<nums.length; i++){
+//         for(int j=i+1; j<nums.length; j++){
+//             if(nums[i] == nums[j]){
+//                 return true;
+//             }
+//             }
+//         }
+//          return false;
+
+//          }
+//     }
+
+// Optimal approach - 
+public class Find_Duplicates_in_array {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+
+    }
+}
+
+
